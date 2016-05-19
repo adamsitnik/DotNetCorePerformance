30e53781bf84466324653ee55df29e10cfddfedc
Adam Sitnik <adam.sitnik@gmail.com>
2016-05-19 23:40:11 +0200

    initial project structure

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Columns;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if !CORE
+            var memoryDiagnoser = new BenchmarkDotNet.Diagnostics.Windows.MemoryDiagnoser();
+
+            BenchmarkRunner
+                .Run<ValueTaskVsTask>(
+                    ManualConfig.CreateEmpty()
+                        //.With(Job.Dry.With(Runtime.Clr).With(Platform.X64).With(Framework.V46).With(Jit.RyuJit).With(Mode.Throughput).WithWarmupCount(1).WithTargetCount(1))
+                        .With(Job.Dry.With(Runtime.Core).With(Platform.X64).With(Jit.RyuJit).With(Mode.Throughput).WithWarmupCount(1).WithTargetCount(1))
+                        .With(DefaultConfig.Instance.GetLoggers().ToArray())
+                        .With(PropertyColumn.Method, PropertyColumn.Runtime, PropertyColumn.Platform, PropertyColumn.Jit, StatisticColumn.Median, BaselineDiffColumn.Scaled, BaselineDiffColumn.Delta)
+                        .With(MarkdownExporter.Default)
+                        .With(HtmlExporter.Default)
+                        .RemoveBenchmarkFiles()
+                        .With(memoryDiagnoser));
+#else
+            throw new InvalidOperationException("Please run this app as NET 4.6 app");
+#endif
+        }
+    }
+}
---
+++ Benchmarks/ValueTaskVsTask.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    public class ValueTaskVsTask
+    {
+        [Benchmark(Baseline = true, Description = "Creating instance of Task")]
+        public Task<ValueTaskVsTask> CreateInstanceOfTask()
+        {
+            return Task.FromResult(this);
+        }
+
+        [Benchmark(Description = "Creating instance of Value Task")]
+        public ValueTask<ValueTaskVsTask> CreateInstanceOfValueTask()
+        {
+            return new ValueTask<ValueTaskVsTask>(this);
+        }
+    }
+}
